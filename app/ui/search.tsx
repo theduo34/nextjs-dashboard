904f51287a9be19102073a066aedad35bbf955a4
Emmanuel <emmsom506@gmail.com>
2024-12-20 12:59:11 +0000

    complete chapter twelve

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -9,9 +9,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace }  = useRouter();
 
-  const handleSearch = useDebouncedCallback((term: string) => {
-      console.log(`Searching..... ${term}`)
-
+  const handleSearch = useDebouncedCallback((term: string)  => {
       const params = new URLSearchParams(searchParams);
       params.set('page', '1')
       if(term) {
