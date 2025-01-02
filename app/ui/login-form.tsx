4f36b29464f1c39b52f934f16775f809dd7dded1
Emmanuel <emmsom506@gmail.com>
2025-01-02 11:06:52 +0000

    complete chapter sixteen

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -72,7 +72,7 @@ export default function LoginForm() {
               <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
               <p className="text-sm text-red-500">{errorMessage}</p>
             </>
-          )};
+          )}
         </div>
       </div>
     </form>
